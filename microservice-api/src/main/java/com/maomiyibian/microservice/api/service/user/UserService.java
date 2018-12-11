44f9bd2066b9a69a991a32b7a46b473f48b1c43a
15173 <15173126889@163.com>
2018-12-11 10:17:20 +0800

    新增用户相关接口

--- microservice-api/src/main/java/com/maomiyibian/microservice/api/service/user/UserService.java
+++ microservice-api/src/main/java/com/maomiyibian/microservice/api/service/user/UserService.java
@@ -15,6 +15,35 @@ public interface UserService {
 
 
     /**
+     * 手机号解绑用户
+     * @param user
+     * @return
+     */
+    boolean unboundUser(User user);
+
+    /**
+     *  手机号绑定用户
+     *  @param user
+     *  @return
+     */
+    boolean boundUser(User user);
+
+
+    /**
+     * 修改用户信息
+     * @param user
+     * @return
+     */
+    boolean updateUserInfo(User user);
+
+    /**
+     * 用户注册
+     * @param user
+     * @return
+     */
+    User userRegister(User user);
+
+    /**
      * 根据用户名查询用户数据
      * @param userName
      * @return
